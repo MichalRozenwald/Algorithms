57f0585c303adab2a8550859e2ce8f09dcfe3370
MichalRozenwald <Michal.rozenwald@gmail.com>
2015-03-15 15:12:59 +0300

    corrected 1-2

--- ДЗ2/1-2_Ways.cpp
+++ ДЗ2/1-2_Ways.cpp
@@ -1,51 +1,46 @@
 #include<iostream>
 #include<vector>
 
-int CountWays(int i, int j,std::vector<std::vector<int>>& data, std::vector<std::vector<int>>& ways) {
-  if(i >= data.size() || j >= data[0].size()) {
+int MOD = 268435459;
+
+int CountWays(int i, int j, const std::vector<std::vector<int>>& net, std::vector<std::vector<int>>& ways) {
+  if(i >= net.size() || j >= net[0].size()) {
     return 0;
   }
 
-  if(data[i][j] == 0) {
+  if(net[i][j] == 0) {
       ways[i][j] = 0;
       return 0;
   }
 
   if(ways[i][j] != -1) { return ways[i][j]; }
-  if(i == data.size() - 1 && j == data[0].size() - 1) {
+  if(i == net.size() - 1 && j == net[0].size() - 1) {
     ways[i][j] = 1;
     return 1;
   }
 
-  ways[i][j] = (CountWays(i, j+1, data, ways) + CountWays(i+1, j, data, ways) ) % 268435459;
+  ways[i][j] = (CountWays(i, j+1, net, ways) + CountWays(i+1, j, net, ways) ) % MOD;
   return ways[i][j];
 }
 
+void input (std::vector< std::vector<int> >& net) {
+  for(int i = 0; i < net.size(); i++){
+    for(int j = 0; j < net[0].size(); j++) {
+      int square;
+      std::cin >> square;
+      //net[i].push_back(square);
+      net[i][j] = square;
+    }
+  }
+}
 
 int main() {
   int n, m;
   std::cin >> n >> m;
-  std::vector<std::vector<int>> data;
-  for(int i = 0; i < n; i++){
-    std::vector<int> line;
-    data.push_back(line);
-    for(int j = 0; j < m; j++) {
-      int square;
-      std::cin >> square;
-      data[i].push_back(square);
-    }
-  }
-
-  std::vector<std::vector<int>> ways;
-  for(int i = 0; i < n; i++){
-    std::vector<int> line;
-    ways.push_back(line);
-    for(int j = 0; j < m; j++) {
-      ways[i].push_back(-1);
-    }
-  }
-
-  std::cout << CountWays(0, 0, data, ways);
+  std::vector< std::vector<int> > net(n, std::vector<int>(m));
+  input(net);
+  std::vector< std::vector<int> > ways(n, std::vector<int>(m, -1));
+  std::cout << CountWays(0, 0, net, ways);
 
   return 0;
 }
