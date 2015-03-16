2d2def997db7c587991417600b5a0ee92f8e372a
MichalRozenwald <Michal.rozenwald@gmail.com>
2015-03-16 17:28:31 +0300

    updated 3

--- ДЗ2/3 - Segments.cpp
+++ ДЗ2/3 - Segments.cpp
@@ -56,16 +56,16 @@ void AddIntersecting(int s1, int s2, Graph& g) {
 }
 
 Graph input() {
-  int n, a, b;
-  std::cin >> n >> a >> b;
-  Graph my_graph(2 * n);
+  int num_of_segm, following, intersecting;
+  std::cin >> num_of_segm >> following >> intersecting;
+  Graph my_graph(2 * num_of_segm);
 
-  for (int pair_a = 0; pair_a < a; pair_a++){
+  for (int fol_pair = 0; fol_pair < following; fol_pair++){
     int segm1, segm2;
     std::cin >> segm1 >> segm2;
     AddFollowing(segm1, segm2, my_graph);
   }
-  for (int pair_b = 0; pair_b < b; pair_b++){
+  for (int inter_pair = 0; inter_pair < intersecting; inter_pair++){
     int segm1, segm2;
     std::cin >> segm1 >> segm2;
     AddIntersecting(segm1, segm2, my_graph);
@@ -73,24 +73,21 @@ Graph input() {
   return my_graph;
 }
 
-void output(std::vector<int>& result) {
-  std::vector<int> coordin(result.size());
-    for (int i = 0; i < result.size(); ++i) {
-      coordin[result[i]] = i;
-    }
-    for (int i = 0; i < result.size(); i += 2) {
-      std::cout << coordin[i] << " " << coordin[i + 1] << std::endl;
-    }
+void output(std::vector<int>& sorted_indexes) {
+  std::vector<int> coordin(sorted_indexes.size());
+  for (int i = 0; i < sorted_indexes.size(); ++i) {
+    coordin[sorted_indexes[i]] = i;
+  }
+  for (int i = 0; i < sorted_indexes.size(); i += 2) {
+    std::cout << coordin[i] << " " << coordin[i + 1] << std::endl;
+  }
 }
 
 int main() {
   Graph my_graph = input();
-  std::vector<int> result = TopSort(my_graph);
-
-  if (result.size() < my_graph.size() / 2) {
-    std::cout << "Impossible";
-  }
-  else { output(result); }
+  std::vector<int> sorted_indexes = TopSort(my_graph);
+  if (sorted_indexes.size() < my_graph.size() / 2) { std::cout << "Impossible"; }
+  else { output(sorted_indexes); }
 
   return 0;
 }
